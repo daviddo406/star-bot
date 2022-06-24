97705bff813224f7de5b89e5d6eca562d098c52b
daviddo406 <phamdavid102@gmail.com>
2022-06-23 23:08:34 -0400

    First Bot
    - followed documentation

--- Starbot/Program.cs
+++ Starbot/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Starbot
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
         {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = File.ReadAllText(@"D:\General\token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
